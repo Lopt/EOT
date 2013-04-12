39aff29d48ef25ab2d57649df61d541882413826
Lopt <leto.larp@googlemail.com>
2013-04-12 16:02:29 +0200

    komplette überarbeitung

--- src/main.py
+++ src/main.py
@@ -1,36 +1,69 @@
 #coding: utf-8
 
-class Timeline():
+ID = 0
+def CreateID():
+    global ID
+    ID += 1
+    return ID
+
+class Change():
+    def __init__(self, obj, function, rfunction, informations):
+        self.function     = function
+        self.informations = informations
+        self.obj          = obj
+        self.rfunction    = rfunction
+        
+    def Apply(self, changes):
+        self.function(*self.informations)
     
-    def __init__(self, parent):
-        self.parent = parent
-        self.active = 0
-        self.timelines = [[], []]
+    def Revert(self, changes):
+        self.rfunction(*self.informations)
+
+class Alternative():
+    def __init__(self, time, timeline):
+        self.time = time
+        self.timeline = timeline
+        self.changes = []
+
+    def AddChange(self, change):
+        self.changes.append(change)
+
+class Time():
     
-    def PrintTimeline(self):
-        for changes in self.timelines:
-            for children in changes:
-                if children:
-                    children.PrintTimeline()
+    def __init__(self):
+        self.alternatives = []
     
-    def ChangeTimeline(self):
-        self.active = not self.active
-    
-class Unit(Timeline):
-    
+    def AddAlternative(self, action):
+        self.alternatives.append(action)
+
+class Unit():
+    def __init__(self, parent, timeline):
+        self.childrens = []
+        self.parent    = parent
+        self.timeline  = timeline
+
     def CreateUnit(self):
-        unit = Unit(self)
-        self.timelines[self.active].append(unit)
-        return unit
-
-    def PrintTimeline(self):
-        Timeline.PrintTimeline(self)
-        print "Test"
-
-
+        return Unit(self, self.timeline)
     
+    def ActivateUnit(self):
+        self.timeline = 1
+    
+    def DeactivateUnit(self):
+        self.timeline = 0
 
-company = Unit(None)
-unit1 = company.CreateUnit()
-unit2 = company.CreateUnit()
 
+time = Time()
+root = Unit(None, CreateID())
+a = root.CreateUnit()
+aa = root.CreateUnit()
+b = root.CreateUnit()
+aChange = Change(a, a.ActivateUnit, a.DeactivateUnit, [])
+aaChange = Change(aa, aa.ActivateUnit, aa.DeactivateUnit, [])
+bChange = Change(b, b.ActivateUnit, b.DeactivateUnit, [])
+
+alternativeT0  = Alternative(0, root.timeline)
+alternativeT0.AddChange(aChange)
+alternativeT0.AddChange(bChange)
+alternativeT0.AddChange(aaChange)
+
+time.AddAlternative(alternative)
