b1762d75c0c7fcfb9fe56ad35420453ccec1aef5
leto <leto.larp@googlemail.com>
2014-05-17 23:14:08 +0000

    replaced output system to curses

--- graphic/output.py
+++ graphic/output.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import copy
-
 from world.world import World
 import constants
 
+import curses
 
-def Output(time):
-    empty_array = [["." for x in range(World.size_x / 1 + 1)] for line in range(World.size_y / 1 + 1)]
-    used_array = copy.deepcopy(empty_array)
 
-    for entity in World.entities[time]:
-        position = entity.Get(time, "Position")
-        if (used_array[int(position.x)][int(position.y)] in constants.NAMES):
-            pass
-        else:
-            used_array[int(position.x)][int(position.y)] = entity.Get(time, "Icon")
-    print 
-    for line in used_array:
-        printed_line = ""
-        for char in line:
-            printed_line += char
-        print printed_line
-    
-    #print "" #"-" * 55
+class Output():
+    def __init__(self):
+        self.empty_array = [" " * World.size_y] * World.size_x
+        self.stdscr = curses.initscr()
+        curses.curs_set(0)
+        self.pad = curses.newpad(World.size_x + 1, World.size_y + 1)
+
+    def curses(self, time):
+        for line_nr in range(len(self.empty_array)):
+            self.pad.addstr(line_nr, 0, self.empty_array[line_nr])
+
+        for entity in World.entities[time]:
+            position = entity.Get(time, "Position")
+            icon = entity.Get(time, "Icon")
+
+            self.pad.addch(int(position.x), int(position.y), ord(icon))
+
+        self.pad.refresh(0,0, 0,0, World.size_x+1,World.size_y+1)
